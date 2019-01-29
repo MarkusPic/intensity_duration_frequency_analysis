020f39e75e7646a148f3451c62f8c39d87e26802
markus <markus.pichler@tugraz.at>
2019-01-29 11:08:04 +0100

    setup py

--- setup.py
+++ setup.py
@@ -5,24 +5,17 @@ __email__ = "markus.pichler@tugraz.at"
 __version__ = "0.1"
 __license__ = "MIT"
 
-from setuptools import setup, find_packages
-
-with open('requirements.txt', 'r') as f:
-    requirements = f.read().splitlines()
+from setuptools import setup
 
 setup(
     name='idf_analysis',
     version='0.1',
-    packages=find_packages(),
-    package_data={
-        # If any package contains *.txt or *.rst files, include them:
-        '': ['*.json', '*.', '*.png', '*.pdf', '*.csv', '*.tex', '*.xslx'],
-    },
+    packages=['idf_analysis'],
     url='https://github.com/MarkusPic/intensity_duration_frequency_analysis',
     license='MIT',
-    author='pichler',
+    author='Markus Pichler',
     author_email='markus.pichler@tugraz.at',
     description='heavy rain as a function of the duration and the return period acc. to DWA-A 531 (2012)',
     scripts=['bin/idf_analysis'],
-    install_requires=requirements,
+    install_requires=['numpy', 'pandas', 'matplotlib', 'tzlocal', 'pytz'],
 )
