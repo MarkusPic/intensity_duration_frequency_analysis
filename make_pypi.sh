47c02887096488552145ce96f7fdbed597418ddb
Markus Pichler <markus.pichler@tugraz.at>
2021-07-22 18:00:14 +0200

    adjust_text for plotting modified influx function

--- make_pypi.sh
+++ make_pypi.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
-python3 setup.py bdist_wheel
+#python3 setup.py bdist_wheel
+#python3 -m twine upload dist/*
+
+# ____________________
+
+SCRIPTPATH=$(dirname "$BASH_SOURCE")
+cd $SCRIPTPATH
+
+python3 -m build
 python3 -m twine upload dist/*
