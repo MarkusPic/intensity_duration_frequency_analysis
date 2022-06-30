93a647fe32e939a40c39bb45c350a91146b5c1f7
Markus Pichler <markus.pichler@tugraz.at>
2022-06-30 22:42:36 +0200

    improved rain plot captions

--- idf_analysis/little_helpers.py
+++ idf_analysis/little_helpers.py
@@ -6,6 +6,7 @@ __version__ = "0.1"
 __license__ = "MIT"
 
 import pandas as pd
+from pandas import Timedelta
 from tqdm.auto import tqdm
 
 from idf_analysis.definitions import COL
@@ -111,13 +112,19 @@ def frame_looper(size, columns, label='return periods'):
 def event_caption(event, unit='mm'):
     caption = 'rain event\n'
     if (COL.START in event) and (COL.END in event):
-        caption += f' between {event[COL.START]:%Y-%m-%d %H:%M} and {event[COL.END]:%Y-%m-%d %H:%M}'
+        caption += f'between {event[COL.START]:%Y-%m-%d %H:%M} and '
+        if f'{event[COL.START]:%Y-%m-%d}' == f'{event[COL.END]:%Y-%m-%d}':
+            caption += f'{event[COL.END]:%H:%M}\n'
+        elif f'{event[COL.START]:%Y-%m-}' == f'{event[COL.END]:%Y-%m-}':
+            caption += f'{event[COL.END]:%d %H:%M}\n'
+        else:
+            caption += f'{event[COL.END]:%Y-%m-%d %H:%M}\n'
 
     if COL.LP in event:
-        caption += f' with a total sum of {event[COL.LP]:0.1f} {unit}'
+        caption += f'with a total sum of {event[COL.LP]:0.1f} {unit}\n'
 
     if COL.DUR in event:
-        caption += f' and a duration of {event[COL.DUR]}'
+        caption += f' and a duration of {timedelta_readable(event[COL.DUR])}'
 
     caption += '.\n'
 
@@ -139,3 +146,61 @@ def return_period_formatter(t):
         return '> 100'
     else:
         return f'{t:0.1f}'
+
+
+def timedelta_components_plus(td, min_freq='T'):
+    """Schaltjahre nicht miteinbezogen"""
+    l = []
+
+    # years, weeks
+    days_year = 365
+    days_week = 7
+
+    for component, value in td.round(min_freq).components._asdict().items():
+        if component == 'days':
+            years, value = value // days_year, value % days_year
+            l.append([int(years), 'years'])
+
+            value -= years // 4
+
+            weeks, value = value // days_week, value % days_week
+            l.append([int(weeks), 'weeks'])
+
+        l.append([value, component])
+    return l
+
+
+def timedelta_components_readable(l, short=False, sep=', '):
+    s = sep.join(
+        ['{}{}{}'.format(v, '' if short else ' ', c[0] if short else (c if v > 1 else c[:-1])) for v, c in l if v > 0])
+    if not short:
+        # replace last "," with "and"
+        s = ' and '.join(s.rsplit(sep, 1))
+    return s
+
+
+def timedelta_readable(td, min_freq='T', short=False, sep=', '):
+    """Schaltjahre nicht miteinbezogen"""
+    return timedelta_components_readable(timedelta_components_plus(td, min_freq), short=short, sep=sep)
+
+
+def timedelta_readable2(d1, d2, min_freq='T', short=False, sep=', '):
+    td = d2 - d1
+
+    years = None
+    if td > Timedelta(days=365):
+        d2_new = d2.replace(year=d1.year)
+
+        if d2_new < d1:
+            d2_new = d2_new.replace(year=d1.year + 1)
+
+        years = d2.year - d2_new.year
+
+        td = d2_new - d1
+
+    l = timedelta_components_plus(td, min_freq)
+
+    if years is not None:
+        l[0][0] = years
+
+    return timedelta_components_readable(l, short=short, sep=sep)
